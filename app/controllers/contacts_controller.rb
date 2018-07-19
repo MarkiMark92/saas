52446d935bacf1c0eeeaa2877bdcbfc987158ee6
Mark Panow <panow.mark@gmail.com>
2018-07-19 14:08:07 +0000

    Contact form und db gehen

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,6 +1,19 @@
 class ContactsController < ApplicationController
-   def new
-   end
+def new
+  @contact = Contact.new
+end
+def create
+  @contact = Contact.new(contact_params)
+  if @contact.save
+     redirect_to new_contact_path, notice: "Nachricht gesendet."
+  else
+     redirect_to new_contact_path, notice: "Etwas ist schiefgelaufen."
+  end
+end
+private
+  def contact_params
+     params.require(:contact).permit(:name, :email, :comment)
+  end
    
 end
    
